04859cee716162f4527fa863de155a79e60de909
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2021-04-14 11:01:20 -0300

    1.1

--- Form1.vb
+++ Form1.vb
@@ -5,28 +5,31 @@
     Private cantOperandos As Integer
     Private operador As Char
     Private operando1, operando2 As Double
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ultimaOrden = "Vacio"
         comaDecimal = False
         cantOperandos = 0
         operador = ""
         operando1 = 0 : operando2 = 0
+
     End Sub
 
     Private Sub btnBorrar_Click(sender As Object, e As EventArgs) Handles btnBorrar.Click
         lblPantalla.Text = lblPantalla.Text.Remove(lblPantalla.Text.Length - 1)
-        'comaDecimal = False'
-        'ultimaOrden = "borrar"'
+        If lblPantalla.Text.Length = 0 Then
+            lblPantalla.Text = "0"
+        End If
     End Sub
 
     Private Sub btnReiniciar_Click(sender As Object, e As EventArgs) Handles btnReiniciar.Click
-        lblPantalla.Text = "0."
+        lblPantalla.Text = "0"
         Form1_Load(Nothing, Nothing)
     End Sub
     'PULSAR 0'
     Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click, btn1.Click, btn2.Click, btn3.Click, btn4.Click, btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click
         Dim numero As String
-        If lblPantalla.Text = "0." And sender.Text = "0" And comaDecimal = False Then
+        If lblPantalla.Text = "0" And sender.Text = "0" And comaDecimal = False Then
             Exit Sub
         End If
         If ultimaOrden <> "digito" Then
@@ -44,7 +47,7 @@
 
     Private Sub coma_Click(sender As Object, e As EventArgs) Handles coma.Click
         If ultimaOrden <> "digito" Then
-            lblPantalla.Text = "0."
+            lblPantalla.Text = "0,"
         ElseIf comaDecimal = False Then
             lblPantalla.Text = lblPantalla.Text & ","
 
@@ -52,19 +55,11 @@
         comaDecimal = True
         ultimaOrden = "digito"
     End Sub
-
-    Private Sub btnPorcentaje_Click(sender As Object, e As EventArgs) Handles btnPorcentaje.Click
-        Dim resultado As Double
-        If ultimaOrden = "digito" Then resultado = operando1 * Val(lblPantalla.Text) / 100
-        If operador = "=" Then
-                resultado = 0
-                lblPantalla.Text = Str(resultado)
-                ultimaOrden = "operador"
-            End If
-        End If
+    Private Sub btnSigno_Click(sender As Object, e As EventArgs) Handles btnSigno.Click
+        lblPantalla.Text = -lblPantalla.Text
     End Sub
 
-    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click, btnSumar.Click, btnRestar.Click, btnMultiplicar.Click, btnDividir.Click, btnPotencia.Click, btnRaiz.Click, btn1x.Click
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click, btnSumar.Click, btnRestar.Click, btnMultiplicar.Click, btnDividir.Click, btnPotencia.Click, btnRaiz.Click, btn1x.Click, btnPorcentaje.Click
 
         If ultimaOrden = "digito" Then
             cantOperandos = cantOperandos + 1
@@ -80,7 +75,9 @@
                 Case "*" : operando1 = operando1 * operando2
                 Case "/" : operando1 = operando1 / operando2
                 Case "^" : operando1 = operando1 ^ operando2
-                Case "√" : operando1 = operando1 ^ (1 / Val(operando2))
+                Case "√" : operando1 = Math.Pow(operando1, 1 / operando2) '16 a la 1/2 es 4'
+                Case "1/x" : operando1 = 1 / operando1
+                Case "%" : operando1 = (operando1 * Val(operando2)) / 100
 
             End Select
             lblPantalla.Text = Str(operando1)
