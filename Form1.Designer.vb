7fae850a472c389acefd854b54dfe776f429563f
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2021-04-18 22:13:05 -0300

    final

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Label1 = New System.Windows.Forms.Label()
         Me.lblPantalla = New System.Windows.Forms.Label()
         Me.btn1 = New System.Windows.Forms.Button()
@@ -517,9 +518,10 @@ Partial Class Form1
         Me.Controls.Add(Me.btn1)
         Me.Controls.Add(Me.lblPantalla)
         Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Calculadora"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
